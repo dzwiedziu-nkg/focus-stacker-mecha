3fba859a8026bc34345a059f1fda1aefb51329d7
nkg <nkg753@gmail.com>
2022-04-08 19:23:53 +0200

    Mockup of interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid place-items-center" style={{ height: window.innerHeight }}>
+      <div className="card card-compact bg-base-100 shadow-xl">
+        { /* <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure> */ }
+        <div className="card-body">
+          <h2 className="font-bold text-center">Focus Stacker Mecha</h2>
+
+          <div className="grid grid-cols-2 gap-4">
+            <button className="btn btn-outline btn-error col-span-2">Stop</button>
+
+            <button className="btn btn-outline btn-warning">Rewind</button>
+            <button className="btn btn-outline btn-warning">Fast forward</button>
+
+            <button className="btn btn-outline btn-primary">Photo, backward</button>
+            <button className="btn btn-outline btn-primary">Photo, forward</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
