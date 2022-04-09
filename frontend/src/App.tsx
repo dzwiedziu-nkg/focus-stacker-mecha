ba2b9f4f7b43569150df76b081a7c68c09fd9247
nkg <nkg753@gmail.com>
2022-04-09 11:52:41 +0200

    Connect with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,69 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import cn from 'classnames';
+import axios from 'axios';
+import { RewindIcon, StopIcon, PlayIcon } from './icons';
+
+interface ToggleButtonProps {
+  children: React.ReactNode;
+  program: number;
+  current: number;
+  switchProgram: (p:number) => void;
+  className: string;
+}
+
+function ProgramButton({
+  children, program, current, switchProgram, className,
+}: ToggleButtonProps) {
+  const c = cn(
+    'btn gap-2 border-4 hover:border-4',
+    className,
+    current === program ? 'border-black hover:border-black' : 'border-white hover:border-white',
+  );
+
+  return (
+    <button className={c} onClick={() => switchProgram(program)}>
+      {children}
+    </button>
+  );
+}
+
+interface SwitchProgramRequest {
+  'program': number;
+  'change_settings': number;
+  'motor_time'?: number;
+  'motor_pause'?: number;
+  'focus_time'?: number;
+  'exposure_time'?: number;
+  'exposure_pause'?: number;
+}
+
+interface SwitchProgramResponse {
+  'program': number;
+  'message': string;
+  'motor_time'?: number;
+  'motor_pause'?: number;
+  'focus_time'?: number;
+  'exposure_time'?: number;
+  'exposure_pause'?: number;
+}
+
+function useApi() {
+  const [program, setProgram] = useState<number>(5);
+
+  const switchProgram = useCallback((p: number) => {
+    const request: SwitchProgramRequest = { program: p, change_settings: 0 };
+
+    axios.post<SwitchProgramResponse>('/api/program', request).then((response) => {
+      setProgram(response.data.program);
+    });
+  }, []);
+
+  return { program, switchProgram };
+}
 
 function App() {
+  const { program, switchProgram } = useApi();
+
   return (
     <div className="grid place-items-center" style={{ height: window.innerHeight }}>
       <div className="card card-compact bg-base-100 shadow-xl">
@@ -9,13 +72,53 @@ function App() {
           <h2 className="font-bold text-center">Focus Stacker Mecha</h2>
 
           <div className="grid grid-cols-2 gap-4">
-            <button className="btn btn-outline btn-error col-span-2">Stop</button>
+            <ProgramButton
+              className="btn-error btn-lg col-span-2"
+              current={program}
+              program={0}
+              switchProgram={switchProgram}
+            >
+              <StopIcon className="w-6 h-6" />
+              <span>Stop</span>
+            </ProgramButton>
 
-            <button className="btn btn-outline btn-warning">Rewind</button>
-            <button className="btn btn-outline btn-warning">Fast forward</button>
+            <ProgramButton
+              className="btn-warning btn-lg"
+              current={program}
+              program={1}
+              switchProgram={switchProgram}
+            >
+              <RewindIcon className="w-6 h-6" />
+              <span>Rewind</span>
+            </ProgramButton>
+            <ProgramButton
+              className="btn-warning btn-lg"
+              current={program}
+              program={2}
+              switchProgram={switchProgram}
+            >
+              <span>Rewind</span>
+              <RewindIcon className="w-6 h-6 rotate-180" />
+            </ProgramButton>
 
-            <button className="btn btn-outline btn-primary">Photo, backward</button>
-            <button className="btn btn-outline btn-primary">Photo, forward</button>
+            <ProgramButton
+              className="btn-primary btn-lg"
+              current={program}
+              program={3}
+              switchProgram={switchProgram}
+            >
+              <PlayIcon className="w-6 h-6 rotate-180" />
+              <span>Photo</span>
+            </ProgramButton>
+            <ProgramButton
+              className="btn-primary btn-lg"
+              current={program}
+              program={4}
+              switchProgram={switchProgram}
+            >
+              <span>Photo</span>
+              <PlayIcon className="w-6 h-6" />
+            </ProgramButton>
           </div>
         </div>
       </div>
---
+++ frontend/src/icons.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable max-len */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+export function RewindIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20.465 20.465" stroke="none" {...props}>
+      <path
+        d="M11.354,1.373c-0.141-0.069-0.304-0.049-0.425,0.044L0.152,9.918C0.059,9.996,0,10.11,0,10.231 s0.059,0.24,0.152,0.316l10.776,8.5c0.073,0.057,0.163,0.086,0.249,0.086l0.176-0.04c0.138-0.064,0.225-0.205,0.225-0.36v-17 C11.578,1.582,11.491,1.438,11.354,1.373z"
+      />
+      <path
+        d="M20.242,1.373c-0.142-0.069-0.305-0.049-0.426,0.044L9.039,9.918 c-0.094,0.078-0.152,0.192-0.152,0.313s0.059,0.24,0.152,0.316l10.777,8.5c0.072,0.057,0.162,0.086,0.248,0.086l0.178-0.04 c0.137-0.064,0.223-0.205,0.223-0.36v-17C20.465,1.582,20.379,1.438,20.242,1.373z"
+      />
+    </svg>
+  );
+}
+
+export function StopIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 10.334 10.334" stroke="none" {...props}>
+      <path
+        d="M10.333,9.816c0,0.285-0.231,0.518-0.517,0.518H0.517C0.233,10.334,0,10.102,0,9.816V0.517 C0,0.232,0.231,0,0.517,0h9.299c0.285,0,0.517,0.231,0.517,0.517V9.816z"
+      />
+    </svg>
+  );
+}
+
+export function PlayIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 368.00177 368.00177" stroke="none" {...props}>
+      <path
+        d="m 60.983181,368.00177 a 35.37,35.37 0 0 1 -17.5,-4.67 c -12,-6.8 -19.46,-20 -19.46,-34.33 V 39.001767 c 0,-14.37 7.46,-27.53 19.46,-34.3300002 a 35.13,35.13 0 0 1 35.77,0.45 L 327.10318,153.48177 a 36,36 0 0 1 0,61 l -247.889999,148.4 a 35.5,35.5 0 0 1 -18.23,5.12 z"
+      />
+    </svg>
+  );
+}
